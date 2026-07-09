d2785abd45629a6168defc0b1af5e840f15da94a
Johao Crespo <johao1999crespo@hotmail.com>
2026-07-09 15:10:54 -0500

    chore(env): point backend url to local cloudflare tunnel
    Allows testing backend integrations with local dev server over Cloudflare quick tunnel.

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,4 +5,5 @@
 export const CLERK_PUBLISHABLE_KEY = "pk_test_dHJ1ZS1tZWVya2F0LTUxLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 //export const BACKEND_URL = "https://rapazos-orientago-backend.hf.space";
-export const BACKEND_URL = "http://192.168.1.104:8000";
+//export const BACKEND_URL = "http://192.168.1.104:8000";
+export const BACKEND_URL = "https://brighton-playing-departmental-advisory.trycloudflare.com";
