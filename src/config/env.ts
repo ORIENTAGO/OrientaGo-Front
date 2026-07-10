a3609a1ce07a628c114f272419dfa85f6da29aed
Johao Crespo <johao1999crespo@hotmail.com>
2026-07-09 21:22:26 -0500

    chore(env): update backend url to Cloudflare tunnel
    Points BACKEND_URL to performer-than-consortium-observation.trycloudflare.com.

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,4 @@ export const CLERK_PUBLISHABLE_KEY = "pk_test_dHJ1ZS1tZWVya2F0LTUxLmNsZXJrLmFjY2
 
 //export const BACKEND_URL = "https://rapazos-orientago-backend.hf.space";
 //export const BACKEND_URL = "http://192.168.1.104:8000";
-export const BACKEND_URL = "https://raymond-platforms-harder-things.trycloudflare.com";
+export const BACKEND_URL = "https://performer-than-consortium-observation.trycloudflare.com";
